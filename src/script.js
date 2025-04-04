6f7f5cdca91a4612b578f41f0354b241edacfbbb
shoaib inamdar <shoaibinamdar14@gmail.com>
2025-04-04 20:34:41 +0530

    features page using frames

--- src/script.js
+++ src/script.js
@@ -105,139 +105,100 @@ const tl = gsap.timeline({
         delay:1
         // stagger: 0.2,
     },'a');
-    
+    tl.to(".products-wrapper>h1>span", {
+      opacity: 0,
+      x: -200,
+      stagger: {
+        each: 0.1,
+        from: "start",
+      },
+      scrollTrigger: {
+        scroller: "body",
+        trigger: ".products-wrapper",
+        start: "top -670%",
+        end: "top 0%",
+        scrub: 1,
+        // markers: true,
+      },
+    });
 
 
-//   const canvas = document.querySelector("#frame");
-//   const context = canvas.getContext("2d");
-//   const frames = {
-//           currentindex: 0,
-//           maxindex: 1345
-//       }
-//       var images = [];
-//       var imageloaded = 0;
-//       function preloader() {
-//           for (var i = 1;i <= frames.maxindex;i++) {
-//               const imageurl = `./images/frame_${i.toString().padStart(4, "0")}.jpeg`
-//               const img = new Image();
-//               img.src = imageurl
-//               img.onload = () => {
-//                   imageloaded++;
-//                   if (imageloaded === frames.maxindex) {
-//                       loadimage(frames.currentindex)
-//                       startanimation()
-//                   }
-//               }
-//               images.push(img)
-//           }
-//       }
+  const canvas = document.querySelector("#frame");
+  const context = canvas.getContext("2d");
+  const frames = {
+          currentindex: 0,
+          maxindex: 1200
+      }
+      var images = [];
+      var imageloaded = 0;
+      function preloader() {
+          for (var i = 1;i <= frames.maxindex;i++) {
+              const imageurl = `./images/frame_${i.toString().padStart(4, "0")}.jpeg`
+              const img = new Image();
+              img.src = imageurl
+              img.onload = () => {
+                  imageloaded++;
+                  if (imageloaded === frames.maxindex) {
+                      loadimage(frames.currentindex)
+                      startanimation()
+                  }
+              }
+              images.push(img)
+          }
+      }
 
-//         function loadimage(index) {
-//             if (index >= 0 && index <= frames.maxindex) {
-//                 const img = images[index];
-//                 canvas.width = window.innerWidth;
-//                 canvas.height = window.innerHeight;
+        function loadimage(index) {
+            if (index >= 0 && index <= frames.maxindex) {
+                const img = images[index];
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight;
 
-//                 const scaleX = canvas.width / img.width
-//                 const scaleY = canvas.height / img.height
-//                 const scale = Math.max(scaleX, scaleY);
+                const scaleX = canvas.width / img.width
+                const scaleY = canvas.height / img.height
+                const scale = Math.max(scaleX, scaleY);
 
-//                 const newwidth = img.width * scale;
-//                 const newheight = img.height * scale;
+                const newwidth = img.width * scale;
+                const newheight = img.height * scale;
 
-//                 const offsetX = (canvas.width - newwidth) / 2;
-//                 const offsetY = (canvas.height - newheight) / 2;
+                const offsetX = (canvas.width - newwidth) / 2;
+                const offsetY = (canvas.height - newheight) / 2;
 
-//                 context.clearRect(0, 0, canvas.width, canvas.height);
-//                 context.imageSmoothingEnabled = true;
-//                 context.imageSmoothingQuality = "high";
-//                 context.drawImage(img, offsetX, offsetY, newwidth, newheight);
-//                 frames.currentindex = index;
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                context.imageSmoothingEnabled = true;
+                context.imageSmoothingQuality = "high";
+                context.drawImage(img, offsetX, offsetY, newwidth, newheight);
+                frames.currentindex = index;
 
-//             }
-//         }
+            }
+        }
 
-//         function startanimation() {
-//             var tl = gsap.timeline({
-//                 scrollTrigger: {
-//                     trigger: ".parent",
-//                     start: "top top",
-//                     scrub: 3,
-//                     // markers:true,
-//                 }
-//             })
-//             function updateframe(index){
-//                 return{
-//                     currentindex:index,
-//                     ease:"linear",
-//                     onUpdate: function () {
-//                         loadimage(Math.floor(frames.currentindex))
-//                     }
-//                 }
-//             }
-//             tl
-//             .to(frames,updateframe(100),'first')
-//             .to(".animate1",{opacity:0,ease:'linear'},'first')
+        function startanimation() {
+            var tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: ".intro",
+                    start: "top top",
+                    end: "200% top",
+                    scrub: 2,
+                    pin:true,
+                    // markers:true,
+                }
+            })
+            function updateframe(index){
+                return{
+                    currentindex:index,
+                    ease:"linear",
+                    onUpdate: function () {
+                        loadimage(Math.floor(frames.currentindex))
+                    }
+                }
+            }
+            tl
             
-//             .to(frames,updateframe(200),"second")
-//             .to(".animate2",{opacity:1,ease:'linear'},'second')
+            .to(frames,updateframe(1201),"16th")
+        }
+        preloader();
+        window.addEventListener("resize",function(){
+            loadimage(Math.floor(frames.currentindex))
+        })
 
-//             .to(frames,updateframe(220),"third")
-//             .to(".animate2",{opacity:1,ease:'linear'},"third")
-//             // .to(".animate2",{opacity:0,ease:'linear'},'third')
-//             .to(frames,updateframe(260),"fourth")
-//             .to(".animate2",{opacity:0,ease:'linear'},"fourth")
-
-//             .to(frames,updateframe(270),"fifth")
-//             .to(".animate3",{opacity:1,ease:'linear'},"fifth")
-
-//             .to(frames,updateframe(330),"sixth")
-//             .to(".animate3",{opacity:0,ease:'linear'},"sixth")
-
-//             .to(frames,updateframe(360),"seventh")
-//             .to(".animate3",{opacity:0,ease:'linear'},"seventh")
-
-//             .to(frames,updateframe(380),"eight")
-//             .to(".panel",{x:'0%',ease:'expo'},"eight")
-
-//             .to(frames,updateframe(400),"ninth")
-//             .to(".panel",{x:'100%',ease:'linear'},"ninth")
-
-//             .to(frames,updateframe(430),"tenth")
-//             .to(".panel",{x:'100%',ease:'linear'},"tenth")
-
-//             .to(frames,updateframe(460),"eleventh")
-//             .to("canvas",{scale:.5,ease:'linear'},"eleventh")
-            
-//             .to(frames,updateframe(480),"twelveth")
-//             .to(".panelism",{opacity:1,ease:'expo'},"twelveth")
-            
-//             .to(frames,updateframe(500),"twelveth")
-//             .to(".panelism span",{width:190,ease:'expo'},"twelveth")
-            
-//             .to(frames,updateframe(530),"13th")
-//             .to("canvas",{scale:1,ease:'linear'},"13th")
-
-//             .to(frames,updateframe(550),"14th")
-//             .to(".panelism",{scale:2,ease:'circ'},"14th")
-
-//             .to(frames,updateframe(570),"15th")
-//             .to(".panelism",{scale:2,ease:'circ'},"15th")
-//         }
-//         preloader();
-//         window.addEventListener("resize",function(){
-//             loadimage(Math.floor(frames.currentindex))
-//         })
-//         document.querySelectorAll(".headings h3")
-//         .forEach(function(elem){
-//             gsap.from(elem,{
-//                 scrollTrigger:{
-//                     trigger:elem,
-//                     start:"top 90%",
-//                     end:'bottom 20%',
-//                     scrub:2
-//                 },
-//                 opacity:.3
-//             })
-//         })
-        // loadimage()
+        loadimage()
