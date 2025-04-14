15d26d755fa0452446b43793098e34cb072605ff
shoaib inamdar <shoaibinamdar14@gmail.com>
2025-04-14 21:15:01 +0530

    features,e-waste,about us,footer added

--- src/script.js
+++ src/script.js
@@ -1,6 +1,22 @@
+import { data } from "autoprefixer";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Lenis from 'lenis';
+import { root } from "postcss";
+import { DoubleSide } from "three";
+import { FullScreenQuad, RectAreaLightUniformsLib } from "three/examples/jsm/Addons.js";
+import { deepCloneAttribute } from "three/examples/jsm/utils/BufferGeometryUtils.js";
+import { element, triplanarTexture } from "three/tsl";
+// import FeatherScroll from "scrollfeather"
+
+import LocomotiveScroll from 'locomotive-scroll';
+
+const locomotiveScroll = new LocomotiveScroll({
+  lenisOptions:{
+    lerp:.05,
+    smoothWheel:true
+  }
+});
 
 
 gsap.registerPlugin(ScrollTrigger) 
@@ -20,22 +36,67 @@ function lenis(){
   requestAnimationFrame(raf);
   }
   
-lenis()
+// lenis()
+// const scroll = new FeatherScroll();
+
+function cursor(){
+  window.addEventListener("mousemove",function(e){
+
+      gsap.to(".cursor",{
+          x:e.clientX,
+          y:e.clientY,
+          opacity:1,
+          // skew:,
+          // ease:"expo.inOut",
+          stagger:.005,
+          // duration:.2
+      })
+  })
+}
+cursor()
+var cursors=document.querySelector(".cursor")
+document.querySelectorAll(".cursoreffect").forEach(function(e){
+    e.addEventListener("mouseenter",function(elem){
+        const data=`${e.getAttribute("data-text")}`
+        if(data=="null"){
+            cursors.innerHTML=""
+        }
+        else{
+            cursors.innerHTML=data
+        }
+        // data.value===null?cursors.innerHTML="":cursors.innerHTML=data
+        // cursors.innerHTML=data
+        // console.log(e.getAttribute("data-text"))
+        gsap.to(".cursor",{
+            scale:7
+        })
+    })
+})
+document.querySelectorAll(".cursoreffect").forEach(function(e){
+    e.addEventListener("mouseleave",function(elem){
+        cursors.innerHTML=""
+        gsap.to(".cursor",{
+            scale:1
+        })
+    })
+})
 
 
 //   var maintl=gsap.timeline();
   const cluttertext = (element) => {
-    const htmlTag = document.querySelector(element);
+    const Tag = document.querySelector(element);
     let clutter = "";
   
-    htmlTag.textContent.split("").forEach((e) => {
+    Tag.textContent.split("").forEach((e) => {
+      if(e==="") clutter+=`<span>$nbsp;</span>`
       clutter += `<span class="inline-block">${e}</span>`;
     });
   
     // Updating the HTML content of the element with the animated spans
-    htmlTag.innerHTML = clutter;
+    Tag.innerHTML = clutter;
   };
   cluttertext(".products-wrapper>h1");
+function products(){
   gsap.from(".products-wrapper>h1>span", {
     opacity: 0,
     x: 200,
@@ -121,8 +182,187 @@ const tl = gsap.timeline({
         // markers: true,
       },
     });
+    
+// var productimg=document.querySelectorAll(".products-imgs")
+// var productimgh1=document.querySelectorAll(".products-img>h1")
+cluttertext(".product1");
+cluttertext(".product2");
+cluttertext(".product3");
+cluttertext(".product4");
+cluttertext(".product5");
+cluttertext(".product6");
 
 
+const allImgs = document.querySelectorAll(".product-imgs");
+allImgs.forEach((img, index) => {
+  img.addEventListener("mouseenter", () => {
+    if (index === 0) {
+      gsap.to("#overlay1", {
+        opacity: 1,
+      });
+      gsap.to(".product1>span", {
+        opacity: 1,
+        // x:200,
+        scale: 1,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    } else if (index === 1) {
+      gsap.to("#overlay2", {
+        opacity: 1,
+      });
+      gsap.to(".product2>span", {
+        opacity: 1,
+        scale: 1,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    } else if (index === 2) {
+      gsap.to("#overlay3", {
+        opacity: 1,
+      });
+      gsap.to(".product3>span", {
+        opacity: 1,
+        scale: 1,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    } else if (index === 3) {
+      gsap.to("#overlay4", {
+        opacity: 1,
+      });
+      gsap.to(".product4>span", {
+        opacity: 1,
+        scale: 1,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    } 
+    else if(index===4){
+      gsap.to("#overlay5", {
+        opacity: 1,
+      });
+      gsap.to(".product5>span", {
+        opacity: 1,
+        scale: 1,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    }
+    else{
+      gsap.to("#overlay6", {
+        opacity: 1,
+      });
+      gsap.to(".product6>span", {
+        opacity: 1,
+        scale: 1,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    }
+  });
+  img.addEventListener("mouseleave", () => {
+    if (index === 0) {
+      gsap.to("#overlay1", {
+        opacity: 0,
+        delay: 0.5,
+      });
+      gsap.to(".product1>span", {
+        opacity: 0,
+        scale: 0,
+        // x:-200,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    } else if (index === 1) {
+      gsap.to("#overlay2", {
+        opacity: 0,
+        delay: 0.5,
+      });
+      gsap.to(".product2>span", {
+        opacity: 0,
+        scale: 0,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    } else if (index === 2) {
+      gsap.to("#overlay3", {
+        opacity: 0,
+        delay: 0.5,
+      });
+      gsap.to(".product3>span", {
+        opacity: 0,
+        scale: 0,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    } else if (index === 3) {
+      gsap.to("#overlay4", {
+        opacity: 0,
+        delay: 0.5,
+      });
+      gsap.to(".product4>span", {
+        opacity: 0,
+        scale: 0,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    } 
+    else if(index===4) {
+      gsap.to("#overlay5", {
+        opacity: 0,
+        delay: 0.5,
+      });
+      gsap.to(".product5>span", {
+        opacity: 0,
+        scale: 0,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    }
+    else {
+      gsap.to("#overlay6", {
+        opacity: 0,
+        delay: 0.5,
+      });
+      gsap.to(".product6>span", {
+        opacity: 0,
+        scale: 0,
+        stagger: {
+          amount: 0.3,
+          from: "start",
+        },
+      });
+    }
+  });
+});
+}
+products()
+
+
+function frames(){
   const canvas = document.querySelector("#frame");
   const context = canvas.getContext("2d");
   const frames = {
@@ -133,7 +373,7 @@ const tl = gsap.timeline({
       var imageloaded = 0;
       function preloader() {
           for (var i = 1;i <= frames.maxindex;i++) {
-              const imageurl = `./images/frame_${i.toString().padStart(4, "0")}.jpeg`
+              const imageurl = `./frames/frame_${i.toString().padStart(4, "0")}.jpeg`
               const img = new Image();
               img.src = imageurl
               img.onload = () => {
@@ -153,8 +393,8 @@ const tl = gsap.timeline({
                 canvas.width = window.innerWidth;
                 canvas.height = window.innerHeight;
 
-                const scaleX = canvas.width / img.width
-                const scaleY = canvas.height / img.height
+                const scaleX = canvas.width / img.width;
+                const scaleY = canvas.height / img.height;
                 const scale = Math.max(scaleX, scaleY);
 
                 const newwidth = img.width * scale;
@@ -175,9 +415,9 @@ const tl = gsap.timeline({
         function startanimation() {
             var tl = gsap.timeline({
                 scrollTrigger: {
-                    trigger: ".intro",
+                    trigger: ".features",
                     start: "top top",
-                    end: "200% top",
+                    end: "100% top",
                     scrub: 2,
                     pin:true,
                     // markers:true,
@@ -202,3 +442,55 @@ const tl = gsap.timeline({
         })
 
         loadimage()
+}
+frames()
+
+function para_effect(){
+  var clutter="";
+document.querySelector(".para-effect")
+.textContent.split("")
+.forEach(function(e){
+  if(e==="") clutter+=`<span>$nbsp;</span>`
+  clutter+=`<span>${e}</span>`;
+})
+document.querySelector(".para-effect").innerHTML=clutter;
+var tl3=gsap.timeline({
+  scrollTrigger:{
+      trigger:".about",
+      scrub:2,
+      start:"50% bottom",
+      end:"bottom 80%",
+      // pin:true,
+      // markers:true
+  }
+});
+gsap.set(".para-effect span",{
+  opacity:.01
+})
+tl3.to(".para-effect span",{
+  opacity:1, 
+  stagger:.03,
+  ease:"power4",
+},'para')
+// tl3.to("#leftimg",{
+//   x:1000
+// },'para')
+
+}
+para_effect()
+
+function ewaste(){
+  gsap.to(".ewaste-wrapper",{
+    x:"-200vw",
+    scrollTrigger:{
+      trigger:".ewaste",
+      scroller:"body",
+      scrub:2,
+      start:"0% 0%",
+      end:"bottom top",
+      pin:true,
+      markers:true
+    }
+  })
+}
+// ewaste()
